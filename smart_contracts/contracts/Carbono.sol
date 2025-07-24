1aa2862184926b45cafe8b3019809c63fc185807
Malkai <omalkai14@gmail.com>
2025-07-24 10:16:10 -0300

    branch criado para finalizar o projeto pronametro

---
+++ smart_contracts/contracts/Carbono.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Carbon {
+    int private value = 0;
+    address private org;
+
+    constructor(address owner) {
+        org = owner;
+    }
+
+    function setprice(int evalue) public {
+        require(
+            msg.sender == org,
+            "Somente a organizacao autorizada pode definir o preco."
+        );  
+        value = evalue;
+    }
+
+    function getprice() public view returns (int) {
+        return (value);
+    }
+}
+
+//event obj
+
+//lista obj
